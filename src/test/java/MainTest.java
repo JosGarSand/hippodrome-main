898a1e021ee8704683fda79973c66fb138f8323d
Jose Garcia Sandoval <g_garcia_jm@hotmail.com>
2025-08-19 14:33:40 -0600

    -Tested all methods in main -Added maven dependencies

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainTest {
+    @Test
+    @Timeout(value = 22, unit = TimeUnit.SECONDS)
+    @Disabled("This test is disabled because it requires a long-running process to complete.")
+    public void methodExecutesWithinTimeout() throws Exception {
+        Main.main(new String[]{});
+    }
 }
