fb5d7b3c360fd063490aa2c28c0c8355da2793ea
Jose Garcia Sandoval <g_garcia_jm@hotmail.com>
2025-08-20 13:11:43 -0600

    -Added logger in non-Testing classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
+
+
         List<Horse> horses = List.of(
                 new Horse("Houdini", 2.4),
                 new Horse("Comanche", 2.5),
@@ -15,6 +21,9 @@ public class Main {
         );
         Hippodrome hippodrome = new Hippodrome(horses);
 
+        logger.info("Starting the race. Number of horses: {}", horses.size());
+
+
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
             watch(hippodrome);
@@ -22,7 +31,11 @@ public class Main {
         }
 
         String winnerName = hippodrome.getWinner().getName();
+
+
+        logger.info("End of the race. The winner is: {}", winnerName);
         System.out.println("The winner is " + winnerName + "!");
+
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
